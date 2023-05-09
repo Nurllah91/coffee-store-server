c9e1821b1121f9c43656e0ed932e0c6c24d49522
nurullah91 <mdnurullah.ptk@gmail.com>
2023-05-09 11:43:15 +0600

    delete form database added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -51,6 +51,13 @@ async function run() {
         res.send(result);
     })
 
+
+    app.delete('/coffee/:id', async(req, res) =>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await coffeeCollection.deleteOne(query)
+        res.send(result);
+    })
 
 
         await client.db("admin").command({ ping: 1 });
